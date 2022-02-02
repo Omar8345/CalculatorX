4d6abf77e86f11fd0809c0ffd6f43b0bba4e7a83
Omar <learnpythonwithomar@gmail.com>
2022-02-02 21:02:44 +0400

    Added main code and policy text by me

---
+++ calculator.py
@@ -0,0 +1,34 @@
+import tkinter #import tkinter
+
+window = tkinter.Tk() # create tkinter master window
+window.geometry('400x300') # set window size
+window.title('My Calculator')
+
+# create buttons for the calculator tkinter gui
+
+button1=tkinter.Button(window, text=" 1 ", width=10, height=10) # create button 1
+button1.grid(row=3,column=1) # arrange button 1 place
+
+button2=tkinter.Button(window, text=" 2 ", width=10, height=10) # create button 2
+button2.grid(row=3,column=1) # arrange button 2 place
+
+button3=tkinter.Button(window, text=" 3 ", width=10, height=10) # create button 3
+button3.grid(row=3,column=1) # arrange button 3 place
+
+button4=tkinter.Button(window, text=" 4 ", width=10, height=10) # create button 4
+button4.grid(row=3,column=1) # arrange button 4 place
+
+button5=tkinter.Button(window, text=" 1 ", width=10, height=10) # create button 5
+button5.grid(row=3,column=1) # arrange button 5 place
+
+button6=tkinter.Button(window, text=" 1 ", width=10, height=10) # create button 6
+button1.grid(row=3,column=1) # arrange button 6 place
+
+button7=tkinter.Button(window, text=" 1 ", width=10, height=10) # create button 7
+button1.grid(row=3,column=1) # arrange button 7 place
+
+button8=tkinter.Button(window, text=" 1 ", width=10, height=10) # create button 8
+button1.grid(row=3,column=1) # arrange button 8 place
+
+button9=tkinter.Button(window, text=" 1 ", width=10, height=10) # create button 9
+button1.grid(row=3,column=1) # arrange button 9 place
